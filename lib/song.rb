862f69c12d219001ac83bcef55fc50d4b8309954
Alexander Baker <alex.baker685@gmail.com>
2019-01-10 04:00:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -31,21 +31,17 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-
-    else
-      self.create_by_name(name)
-    end
+   self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
     self.all.sort_by { |song| song.name }
   end
 
-  def self.create_from_filename(filename)
+  def self.new_from_filename(filename)
 
-    filename.chomp(".mp3")
-    artist_name_song_name = filename.split("-")
+    combined_name = filename.chomp(".mp3")
+    artist_name_song_name = combined_name.split("-").join.split("  ")
 
     artist_name = artist_name_song_name[0]
     song_name = artist_name_song_name[1]
@@ -54,6 +50,23 @@ class Song
     song.name = song_name
     song.artist_name = artist_name
     song
+  end
+
+
+  def self.create_from_filename(filename)
+
+    combined_name = filename.chomp(".mp3")
+    artist_name_song_name = combined_name.split("-").join.split("  ")
+
+    artist_name = artist_name_song_name[0]
+    song_name = artist_name_song_name[1]
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song.save
+    song
+
   end
 
   def self.destroy_all
