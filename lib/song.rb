a02a37c5ff494bfdb41637bb118bc5021797cd54
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-29 18:51:24 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,48 @@ class Song
     self.class.all << self
   end
 
+  def initialize(name,artist)
+      @name = name
+      @artist =
+      @@all << self
+  end
+
+
+  def create
+
+  end
+
+  def new_by_name
+
+  end
+
+  def create_by_name
+
+  end
+
+  def find_by_name
+
+  end
+
+  def find_or_create_by_name
+
+  end
+
+  def alphabetical
+
+  end
+
+  def new_from_filename
+
+  end
+
+  def create_from_filename
+
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
 end
