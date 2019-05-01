f345246cacee37a8c4b93e5e5ae84d639090183b
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-01 03:17:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,65 @@ class Song
 
   def save
     self.class.all << self
+
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|i| i.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if !self.find_by_name(name)
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song_attr = filename.split(" - ")
+    song = self.new
+    song.artist_name = song_attr[0]
+    song.name = song_attr[1].split(".")[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    #binding.pry
+    song_attr = filename.split(" - ")
+    song = self.new
+    song.artist_name = song_attr[0]
+    song.name = song_attr[1].split(".")[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
