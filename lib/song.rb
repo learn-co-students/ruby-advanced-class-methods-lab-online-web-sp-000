0c42241dde4a332c7a0af17f8010a7a241dc8920
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-04 22:15:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,24 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
-    @@all
+   @@all
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
   end
 
   def save
     self.class.all << self
   end
 
+  def self.new_by_name
+    self.all.each do |song|
+      song.name = song.name.split(" ").collect{|s| s.capitalize}.join(" ")
+    end
+  end
 end
