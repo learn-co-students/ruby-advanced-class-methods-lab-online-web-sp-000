fd5579ada1e2f6b86f4b069f0b9943787f4c0004
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-14 23:34:47 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,25 +2,6 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.create
-    song = self.new
-    song.save
-    song
-  end
-
-  def self.new_by_name(name)
-    song = self.new
-    @name = name
-    song.name = name
-    song
-  end
-
-  def self.create_by_name(name)
-    song = self.new
-    @name = name
-    song.name = name
-    song
-  end
 
   def self.all
     @@all
@@ -29,6 +10,26 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+
 
   def self.find_by_name(search)
     @@all.find do |song|
@@ -44,9 +45,9 @@ end
     self.find_by_name(name) || self.create_by_name(name)
   end
 
-  def self.alphebetical
-    self.all.sort_by { |song| song.name}
-  end
+  def self.alphabetical
+     self.all.sort_by {|song| song.name }
+   end
 
   def self.new_from_filename(filename)
     title = filename.split(" - ")
