2fca1f48f24c699b333a53f7024441d358fb3713
Mark Harvey <maharvey2@crimson.ua.edu>
2019-06-04 02:11:16 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     self.class.all << self
   end
 
-#this part creates a new song
+#this part creates a new song and saves to the Song.all part
   def self.create
     song = self.new
     song.save
@@ -22,7 +22,7 @@ class Song
     song.name = song_name
     song
   end
-
+#just the song name
   def self.create_by_name(song_name)
     song = self.create
     song.name = song_name
@@ -32,15 +32,15 @@ class Song
   def self.find_by_name(song_name)
     self.all.detect{|s| s.name == song_name}
   end
-
+#prevents dupes by looking for the song or making one
   def self.find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
-
+#sort by Enumerator
   def self.alphabetical
     self.all.sort_by{|s| s.name}
   end
-
+#pull by filename. splits things on other side of - breaks down to array 0 = TS and 1 Blank
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
@@ -62,7 +62,7 @@ class Song
     song.artist_name = artist_name
     song
 end
-
+#this clears out the @@all array where all this is stored
   def self.destroy_all
     self.all.clear
 end
