5eb2d159c9d06bc0d81bf6b90d703e7e04c0d0d0
Becky Phan <beckyphan@live.com>
2019-06-05 22:47:38 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,12 +30,7 @@ class Song
   end
   
   def self.find_by_name(name)
-    @@all.each do |song|
-      if song.name == name
-        return song
-      end 
-    end
-    nil
+    @@all.detect {|song| song.name == name}
   end
   
   def self.find_or_create_by_name(name)
