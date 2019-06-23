160952356a918b18e0bc532b89d5ce983a6ab250
github username <github email address>
2019-06-23 09:47:24 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,14 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def initialize(name, artist = nil)
+    @name = name
+    @artist_name = artist
+    @@all << self
+  end
 
   def self.all
     @@all
@@ -8,6 +16,67 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    Song.new(name)
+    @@all[-1]
+  end
+
+  def self.new_by_name(name)
+    Song.new(name)
+    @@all[-1]
+  end
+
+  def self.create_by_name(name)
+    Song.new(name)
+  end
+
+  def self.find_by_name(name)
+    if @@all.length > 0
+      @@all.each do |instance|
+        if instance.name == name
+          return instance
+        else
+          nil
+        end
+      end
+    else
+      nil
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by do |instance|
+      instance.name
+    end
+  end
+
+  def self.new_from_filename(file_name)
+    artist_song = file_name.split(" - ")
+    artist = artist_song[0]
+    song_and_file = artist_song[1]
+    split_from_file = song_and_file.split(".")
+    song = split_from_file[0]
+
+    Song.new(song, artist)
+    @@all[-1]
+  end
+
+  def self.create_from_filename(file_name)
+    self.new_from_filename(file_name)
+  end
+
+  def self.destroy_all
+    @@all = []
   end
 
 end
