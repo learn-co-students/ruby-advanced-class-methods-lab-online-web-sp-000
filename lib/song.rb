6e97c0ee592e4130c175f517d06ab62b48ef33f7
Peter Grant <jgrantps@hotmail.com>
2019-06-29 16:18:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,10 +11,10 @@ class Song
   end
 
   def self.create
-    #=>initializes a song and saves it to the @@all class variable
     new_song = self.new
     new_song.save
     new_song
+    #=>initializes a song and saves it to the @@all class variable
   end
 
   def self.new_by_name(name)
@@ -31,66 +31,52 @@ class Song
     new_song.name = name
     new_song.save
     new_song
-
     #=>takes in the string name of a song and returns a song instance with that name set
     #=>as its name property and the song being saved into the @@all class variable.
-
-
   end
 
-
-
   def self.find_by_name(song_name)
+    self.all.detect { |i| i.name == song_name }
     #=>Finder that accepts the string name of a song and
     #=>returns the matching instance of the song with that name.
-    self.all.detect { |i| i.name == song_name }
   end
 
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
-
     #=> accept a string name for a song and either return a matching song instance
     #=>with that name or create a new song with the name and return the song instance.
   end
 
   def self.alphabetical
-    #=> returns all the songs in ascending (a-z) alphabetical order.
     self.all.sort_by { |x| x.name}
+    #=> returns all the songs in ascending (a-z) alphabetical order.
   end
 
   def self.new_from_filename(filename)
-    #=> a class constructor that accepts a filename in the format of " - .mp3",
-    #=> for example, "Taylor Swift - Blank Space.mp3".
     song_name = filename.split(" - ")[1].chomp(".mp3")
     artists_name = filename.split(" - ")[0]
-
-
     new_song = self.new
     new_song.name = song_name
     new_song.artist_name = artists_name
-
     new_song
-
-
+    #=> a class constructor that accepts a filename in the format of " - .mp3",
+    #=> for example, "Taylor Swift - Blank Space.mp3".
   end
 
   def self.create_from_filename(filename)
-    #=>class constructor that accepts a filename in the format of " - .mp3",
-    #=>for example "Taylor Swift - Blank Space.mp3"
     song_name = filename.split(" - ")[1].chomp(".mp3")
     artists_name = filename.split(" - ")[0]
-
-
     new_song = self.new
     new_song.name = song_name
     new_song.artist_name = artists_name
     new_song.save
     new_song
+    #=>class constructor that accepts a filename in the format of " - .mp3",
+    #=>for example "Taylor Swift - Blank Space.mp3"
   end
 
   def self.destroy_all
-    #=>class method should reset the state of the @@all
     self.all.clear
+    #=>class method should reset the state of the @@all
   end
-
 end
