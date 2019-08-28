94b46348b37ef44f220ac43d1705c33ddcbdd287
Austin Asbury <acasbury25@aol.com>
2019-08-28 00:35:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -36,9 +36,29 @@ class Song
   end
 
   def self.find_or_create_by_name (name)
-    if self.find_by_name(name) == false
-      self.create_by_name(name)
-    else
-      self.find_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename (filename)
+    filename_array = filename.split(" - ")
+    new_song = self.new
+    new_song.artist_name = filename_array[0]
+    new_song.name = filename_array[1].chomp(".mp3")
+    new_song
+  end
+
+  def self.create_from_filename (filename)
+    new_song = self.new_from_filename(filename)
+    new_song.save
+    new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
