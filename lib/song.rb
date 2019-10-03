6140586846a9e1fbcc0369aff88ad0728b5cfec7
Blythe Pollard <blytheepollard@gmail.com>
2019-10-03 02:51:42 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -11,11 +13,24 @@ class Song
   end
 
   def self.create
-    @@all << self
-    return self
-end
+    create_self = self.new
+    create_self.save
+    create_self
+  end
+   
+   def self.new_by_name(name) 
+     name
+     #takes in string of a name of an instance
+     #return song instance with that name set as its name property
+    end
 
-  def self.new_by_name(name)
-    @@all.find {|person| person.name == name}
-end
-end
+  def self.create_by_name(name)
+    #name  #takes in string name of a song - name
+    #returns a song instance with that name set as its name property and the song. Instance variable??
+    # @@all << @name #saves to @@all class variable -- @@all << new thing
+  end
+ 
+  def self.find_by_name(name)
+    @@all.find {|n| n.name == name}
+  end
+  end
