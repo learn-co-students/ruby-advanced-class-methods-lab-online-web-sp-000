4ea22f54ed6d84ee23e48cbe73e7b8d4090fa68a
Will Carter <will.carter@fergusllc.com>
2020-01-10 17:57:01 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |song| song.name == name end
+  end
+
+  def self.find_or_create_by_name(name)
+    found_song = self.find_by_name(name)
+    if found_song != nil
+      found_song
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |song| song.name end
+  end
+
+  def self.new_from_filename(file_name)
+    song_array = file_name.gsub!(".mp3", "").split("-")
+    song = self.new
+    song.name = song_array[1].strip
+    song.artist_name = song_array[0].strip
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    song = new_from_filename(file_name)
+    song.save
+  end
+
+  def self.destroy_all()
+    self.all.clear
+  end
+
 end
