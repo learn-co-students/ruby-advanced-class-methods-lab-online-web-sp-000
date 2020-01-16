fe4ee71e0e0bce444e516cf2bc938bf0290c0e41
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-16 22:25:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,15 @@ class Song
   
   def self.create_by_name(title)
     song = self.create
+    song.name = title
+    song
+  end
+  
+  def self.find_by_name(title)
+    self.all.detect{|s| s.name == song_name}
+  end 
+  
+  def self.find_or_create_by_name(title)
     
   end 
 end
