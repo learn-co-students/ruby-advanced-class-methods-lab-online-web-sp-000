ad17968feb0cb492eb9ee859bf368342fbb6e5be
elsa chen <snowing222@gmail.com>
2020-05-28 19:37:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,60 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song=self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song=self.new
+    song.name=name
+    song
+  end
+
+  def self.create_by_name(name)
+    song=self.new
+    song.save
+    song.name=name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name==name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name)||self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song|song.name}
+  end
+   #Song.new_from_filename("Taylor Swift - Blank Space.mp3")
+
+  def self.new_from_filename(name)
+    array=name.gsub(".mp3","").split(" - ")
+    song=self.new
+    song.name=array[1]
+    song.artist_name=array[0]
+    song
+  end
+
+  def self.create_from_filename(name)
+    array=name.gsub(".mp3","").split(" - ")
+    song=self.new
+    song.name=array[1]
+    song.artist_name=array[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
+
+
 end
