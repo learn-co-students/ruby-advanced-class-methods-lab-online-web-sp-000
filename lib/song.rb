6f5c04e3f58559973445c6058f852e9176d7f8d0
Heath Mast <hrmast@gmail.com>
2020-06-04 04:37:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,67 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def name
+    @name
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(file_name)
+    #initializes a song and artist_name based on the filename format (FAILED - 1)
+    song_array = file_name.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear()
   end
 
 end
