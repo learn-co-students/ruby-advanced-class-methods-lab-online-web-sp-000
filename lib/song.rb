bee0dfd7bfd99971c54f367e0a8d7572c6bd7284
Seth Near <seth.j.near@gmail.com>
2020-06-23 15:31:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,43 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    self.new
+    self.save
+  end
+
+  def self.new_by_name(name)
+    self.new
+    self.name = name
+    return self
+  end
+
+def self.create_by_name(name)
+  @@all << self.new_by_name(name)
+end
+
+def self.find_by_name(name)
+
+end
+
+def self.find_or_create_by_name(name)
+
+end
+
+def self.alphabetical
+
+end
+
+def self.new_from_filename(filename)
+
+end
+
+def self.create_from_filename(filename)
+
+end
+
+def self.destroy_all
+  @@all.clear
+end
+
 end
