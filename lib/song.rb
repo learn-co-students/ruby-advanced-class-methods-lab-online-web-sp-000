d6a83069c22e27337d9932ef7d4d0484798cb539
danny galimi <dannygalimi13@aim.com>
2020-09-24 22:03:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,41 @@ class Song
     self.class.all << self
   end
 
+def self.create
+  song = Song.new
+  song.save
+  song
+end
+
+def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  song
+end 
+
+def self.create_by_name(name)
+  
+  song = self.create
+  song.name = name
+  song
+end
+
+def self.find_by_name(name)
+  self.all.find{|p| p.name == name}
+end 
+
+def self.find_or_create_by_name(name)
+  
+  if self.find_by_name(name)
+    return self.find_by_name(name)
+  else 
+    song = self.create_by_name(name)
+   song
+  end
+end
+
+def self.alphabetical
+  @@all.sort_by {|a,b| a.title <=> b.title}
+end
+
 end
