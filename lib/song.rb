bb4c4e6978d7f7d1b8dcdbf67da1722f96de036c
Chris Houghton <chrishoughton2434@gmail.com>
2020-09-26 15:17:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -35,11 +35,11 @@ class Song
   end
       
   def self.find_or_create_by_name(song_name)
-    if self.find_by_name(song_name)
-        self.find_by_name(song_name)
-    else self.create_by_name(song_name)
-      
-    end
+    #if self.find_by_name(song_name)
+        #self.find_by_name(song_name)
+    #else self.create_by_name(song_name)
+      self.find_by_name(song_name) || self.create_by_name(song_name)
+    #end
   end 
   
   def self.alphabetical
