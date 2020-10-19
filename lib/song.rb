4c5eab94c9c670314fe238ac23c3d3a9905f4d12
fut0k0 <cccabell@icloud.com>
2020-10-19 16:46:20 -0700

    Passing lab

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,57 @@
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
+	attr_accessor :name, :artist_name
 
-  def self.all
-    @@all
-  end
+	@@all = []
 
-  def save
-    self.class.all << self
-  end
+	def self.all
+		@@all
+	end
 
+	def self.create
+		self.new.save
+	end
+
+	def self.new_by_name(name)
+		song = self.new
+
+		song.name = name
+
+		song
+	end
+
+	def self.create_by_name(name)
+		self.new_by_name(name).save
+	end
+
+	def self.find_by_name(name)
+		self.all.find{|song| song.name == name}
+	end
+
+	def self.find_or_create_by_name(name)
+		self.find_by_name(name) || self.create_by_name(name)
+	end
+
+	def self.alphabetical
+		self.all.sort_by{|song| song.name}
+	end
+
+	def self.new_from_filename(filename)
+		song = self.new_by_name(filename.split(" - ")[-1].chomp(".mp3"))
+
+		song.artist_name = filename.split(" - ")[0]
+
+		song
+	end
+
+	def self.create_from_filename(filename)
+		self.new_from_filename(filename).save
+	end
+
+	def self.destroy_all
+		self.all.clear
+	end
+
+	def save
+		self.class.all.push(self)[-1]
+	end
 end
