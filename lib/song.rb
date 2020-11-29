297730a2be0262e85a7f53209a217367d041ae3f
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-29 03:18:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -8,6 +9,22 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+
+  end
+
+  def self.new_by_name
+  end
+
+  def self.create_by_name
+  end
+
+  def self.find_by_name
+  end
+
+  def self.find_or_create_by_name
   end
 
 end
