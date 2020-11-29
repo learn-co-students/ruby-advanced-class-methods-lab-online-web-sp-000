1d111f725568681ffccc48cde0091b46ee652cb4
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-29 22:21:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -25,19 +25,56 @@ class Song
 
   def self.create_by_name(name)
     @name = name
-    song = Song.new
+    song = self.create
     song.name = @name
-    song.save
     song
   end
 
   def self.find_by_name(name)
     @name = name
-    @@all.each { |object| return object if object.name == @name }
-    false
+    self.all.find { |object| object.name == @name }
+
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    @name = name
+    self.find_by_name(@name) || self.create_by_name(@name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |object| object.name }
+  end
+
+  def self.from_file(file_name)
+    @file_name = file_name.split(" - ")
+    @artist_name = @file_name[0]
+    @song_name = @file_name[1]
+    @song_name.delete_suffix!(".mp3")
+    [@song_name, @artist_name]
+  end
+
+  def self.new_from_filename(file_name)
+    @file_name = file_name
+    song_name, artist_name = from_file(@file_name)
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    @file_name = file_name
+    song_name, artist_name = from_file(@file_name)
+
+    song = self.create
+    song.name = @song_name
+    song.artist_name = @artist_name
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
