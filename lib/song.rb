085bdd2e25dfeb7c105b46a0121b1ab4de15883f
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-12-06 00:45:47 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,56 @@ class Song
   @@all = []
 
   def self.create
-    @@all << self
-    save
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|p| p.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if Song.find_by_name(name) != true
+      Song.create_by_name(name)
+    end
+    Song.find_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|a| a.name}
+  end
+
+  def self.new_from_filename(filename)
+    fileinfo = filename.split(/ - |.mp3/)
+    artist_name = fileinfo[0]
+    name = fileinfo[1]
+    song = Song.new
+    song.artist_name = artist_name
+    song.name = name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    @@all << song
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
   def self.all
