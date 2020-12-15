49d16295f85debea3f932f504790c8ff40fe3503
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 18:58:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,55 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    song_info = /^(?<artist>.+) - (?<song>.+)\..+$/.match(filename).named_captures
+    song = Song.new
+    song.name = song_info["song"]
+    song.artist_name = song_info["artist"]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_info = /^(?<artist>.+) - (?<song>.+)\..+$/.match(filename).named_captures
+    song = Song.create
+    song.name = song_info["song"]
+    song.artist_name = song_info["artist"]
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
