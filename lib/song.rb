21b3d406fa492941605f67f4598da047d2bcf5cf
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-14 01:30:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,35 +5,27 @@ class Song
 
 def self.create
   new_song = Song.new 
+  new_song.save 
+  new_song 
 end 
-
  
-def initialize
-  @name = name 
-  @artist_name = artist_name 
-  @@name
-  @@artist_name 
-  self.class.all << self
-end
-
-
+ 
 def self.new_by_name(name)
- "Blank Space" = Song.new 
+ new_song = Song.new
+ new_song.name = name 
+ #.name here relates to the attr_accessor name, not a method variable
+ new_song 
 end 
 
   
   def self.all
     @@all
-  end
+  end 
 
-  def self.save
+  def save
     self.class.all << self
   end
 
-
-def self.artist_name 
-  
-end 
 
 def self.find_or_create_by_name(name)
   #it 'invokes .find_by_name and .create_by_name instead of repeating code' do
