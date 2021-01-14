7bbc14756efd77a094c0eb6196eda3f450eeb04b
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-14 01:55:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,18 +6,29 @@ class Song
 def self.create
   new_song = Song.new 
   new_song.save 
+  #to call the save method down below 
   new_song 
+  #to return the new song per the test 
 end 
  
  
 def self.new_by_name(name)
  new_song = Song.new
- new_song.name = name 
- #.name here relates to the attr_accessor name, not a method variable
+    #we are using new_song instead of Song because we earlier changed the value to new_song 
+    new_song.name = name 
+    #.name here relates to the attr_accessor name, not a method variable
+    #we are passing in the argument here 
  new_song 
 end 
 
-  
+def self.create_by_name(name)
+  new_song = Song.new
+  new_song.name = name 
+  new_song.save 
+    new_song
+end 
+
+
   def self.all
     @@all
   end 
@@ -25,6 +36,12 @@ end
   def save
     self.class.all << self
   end
+
+
+def self.find
+  
+end 
+
 
 
 def self.find_or_create_by_name(name)
