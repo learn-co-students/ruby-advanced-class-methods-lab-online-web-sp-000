dc63a78e81173538da496d230d2aa5745d804152
Chris Baum <christopher.baum@valpo.edu>
2021-01-26 02:28:38 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -24,5 +24,46 @@ class Song
     song
   end
 
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|x| x.name}
+  end
+
+  def self.new_from_filename(filename)
+    new_file = filename.split(" - ")  #splits the filename into an array
+    artist_name = new_file[0] #sets artist name to the first array position
+    song_name = new_file[1].gsub(".mp3","") #removes .mp3 from end and sets song name to 2nd array position
+    song = self.new  #creates a new song
+    song.name = song_name #saves song name under the name property
+    song.artist_name = artist_name #saves artist name under the artist name property
+    song
+  end
+
+  def self.create_from_filename(filename)
+    new_file = filename.split(" - ")
+    artist_name = new_file[0]
+    song_name = new_file[1].gsub(".mp3","")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 
 end
