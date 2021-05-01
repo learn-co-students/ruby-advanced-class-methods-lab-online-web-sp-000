623d4aea83f5e45f8c50e366c6188a3e43f770f4
pekellogg <pekellogg@hotmail.com>
2021-04-30 18:13:42 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,54 @@ class Song
     self.class.all << self
   end
 
+  def self.create # class constructor bc this is adding new functionality to the class. this doesn't need a reader.
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name} || nil
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by(&:name)
+  end
+
+  def self.new_from_filename(filename)
+    filename_array = filename.chomp(".mp3").split(" - ")
+    song = Song.new
+    song.artist_name = filename_array[0]
+    song.name = filename_array[1]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    new_song = self.new_from_filename(filename)
+    new_song.save
+    new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
+
+song = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
