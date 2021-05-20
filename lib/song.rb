745ad520cfcb111eca97ffff98049e06d8d8f46f
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-20 21:43:52 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,47 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new #instantiate the song
+    song.save #save the song, saving the class, not the instance
+    song # returns new song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new #instantiate a song with a name
+    song.name=name #define name
+    song #returns a song instance with the name set 
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+    #below this also works
+    #song = self.new #takes in the name of a song
+    #song.name=name #define name
+    #@@all << song #song saved in @@all
+    song #returns a song instance with the name set
+  end
+
+  def self.find_by_name(name) 
+    self.all.find{|song| song.name == name} #find song in @@all
+  end
+  
+  def self.find_or_create_by_name(name) 
+    #if a method serves two purposes you want to branch it into helper methods
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+  end
+  
+  def self.new_from_filename
+  end
+
+  def self.create_from_filename
+  end
+
+  def self.destroy_all
+  end
 end
