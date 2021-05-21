5e7a466a0c0e27e94fb29dade2544b8635a66a22
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-21 16:27:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -35,17 +35,28 @@ class Song
   
   def self.find_or_create_by_name(name) 
      self.find_by_name(name) || self.create_by_name(name)
-    #binding.pry
    end
  
   
   def self.alphabetical
+    @@all.sort_by {|song| song.name}
   end
   
-  def self.new_from_filename
+  def self.new_from_filename(filename)
+    artist_name, song_name = filename.split(" - ")
+    song = self.new_by_name(song_name.gsub(".mp3", ""))
+    song.artist_name = artist_name
+    song
   end
 
-  def self.create_from_filename
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
+  end
+  
+  def self.destroy_all
+    @@all.clear
   end
 
   
